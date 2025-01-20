99061fb8ae1e000d663da44c3c203abf864fff63
InfinixInfotech <infinixinfotech2024@gmail.com>
2025-01-20 15:09:40 +0530

    test

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
     <div className="Login-Conatiner">
       <div className="login-page">
         <div className="welcome-banner">
-          <h1>Welcome for testing  </h1>
+          <h1>check the cicd pipeline</h1>
         </div>
         <div className="login-container">
           <h2>Login</h2>
