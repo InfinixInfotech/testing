60c1339c8e8f3c68e767f0158ad5b5a8a7b415b1
InfinixInfotech <infinixinfotech2024@gmail.com>
2025-01-22 13:56:34 +0530

    updation

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
     <div className="Login-Conatiner">
       <div className="login-page">
         <div className="welcome-banner">
-          <h1>WIN-WEALTH ALGOS SOFTWARE PVT LTD</h1>
+          <h1>WIN-WEALTH ALGOS</h1>
         </div>
         <div className="login-container">
           <h2>Login</h2>
