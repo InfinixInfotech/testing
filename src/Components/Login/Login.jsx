c12e529453a23ec48202762550776745fca07bc1
InfinixInfotech <infinixinfotech2024@gmail.com>
2025-01-23 09:34:33 +0530

    name change

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
     <div className="Login-Conatiner">
       <div className="login-page">
         <div className="welcome-banner">
-          <h1>////WIN-WEALTH ALGOS\\\\</h1>
+          <h1>Infinix-Infotech</h1>
         </div>
         <div className="login-container">
           <h2>Login</h2>
